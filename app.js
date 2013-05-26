a7f21a7ca20e568c81b36c4afac5c89b34044896
P. Mark Anderson <mark@spotmetrix.com>
2013-05-25 19:47:08 -0700

    test

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
+console.log("STARTING TEST NODE")
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	console.log("GET!!!")
+	response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  console.log("done STARTING TEST NODE")
+
 });
